6f9c407b5ca86ce6397a2036ee03c0f17a301a61
JoshuaW13 <josh.walters@shaw.ca>
2024-10-22 12:34:16 -0600

    Updated home page buttons to grow with screen size

--- conceptFitness/src/pages/Home.jsx
+++ conceptFitness/src/pages/Home.jsx
@@ -11,19 +11,19 @@ function Home() {
   return (
       <div className='w-full h-full flex flex-col items-center'>
         <NavBar FirstButton={HomeButton} SecondButton={ProfileButton}></NavBar>
-        <Calender></Calender>
-        <div className='flex flex-col m-4 w-[90%] gap-4'>
-          <div className='flex flex-row gap-2'>
-            <div className='flex flex-col w-[30%] gap-4'>
-              <button className='bg-gray-300 text-sm flex items-center justify-center'>Catalogue</button>
-              <button className='bg-gray-300 text-sm flex items-center justify-center'>Programs</button>
-              <button className='bg-gray-300 text-sm flex items-center justify-center'>Logs</button>
+        <Calender className="flex-grow"></Calender>
+        <div className='flex flex-col m-4 w-[90%] gap-4 flex-grow'>
+          <div className='flex flex-row gap-2 flex-grow'>
+            <div className='flex flex-col w-[30%] gap-4 flex-grow'>
+              <button className='bg-gray-300 text-sm flex items-center justify-center flex-grow'>Catalogue</button>
+              <button className='bg-gray-300 text-sm flex items-center justify-center flex-grow'>Programs</button>
+              <button className='bg-gray-300 text-sm flex items-center justify-center flex-grow'>Logs</button>
             </div>
-            <div className='w-[80%] bg-gray-300 rounded-lg'>
+            <div className='w-[80%] bg-gray-300 rounded-lg flex-grow'>
               Goals
             </div>
           </div>
-          <button className='bg-gray-300'>Start Session</button>
+          <button className='bg-gray-300 flex-grow-[0.25]'>Start Session</button>
         </div>
         
       </div>
