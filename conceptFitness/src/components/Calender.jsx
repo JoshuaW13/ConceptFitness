f0e98e1ab2dfbc4ffbedaa2adffc6d6d7b92f256
JoshuaW13 <josh.walters@shaw.ca>
2024-10-22 12:24:12 -0600

    Added the remainder of the homepage buttons

--- conceptFitness/src/components/Calender.jsx
+++ conceptFitness/src/components/Calender.jsx
@@ -4,7 +4,7 @@ import CalenderImage from "../assets/weeklycalender.png"
 function Calender() {
 
   return (
-      <div className="w-[90%] h-[30%] bg-gray-50 rounded-lg shadow-lg flex items-center justify-center mt-6 border-gray border-2">
+      <div className="w-[90%] h-[35%] bg-gray-50 rounded-lg shadow-lg flex items-center justify-center mt-6 border-gray border-2">
         <img src={CalenderImage} alt="" />
       </div>
   )
--- conceptFitness/src/pages/Home.jsx
+++ conceptFitness/src/pages/Home.jsx
@@ -12,6 +12,20 @@ function Home() {
       <div className='w-full h-full flex flex-col items-center'>
         <NavBar FirstButton={HomeButton} SecondButton={ProfileButton}></NavBar>
         <Calender></Calender>
+        <div className='flex flex-col m-4 w-[90%] gap-4'>
+          <div className='flex flex-row gap-2'>
+            <div className='flex flex-col w-[30%] gap-4'>
+              <button className='bg-gray-300 text-sm flex items-center justify-center'>Catalogue</button>
+              <button className='bg-gray-300 text-sm flex items-center justify-center'>Programs</button>
+              <button className='bg-gray-300 text-sm flex items-center justify-center'>Logs</button>
+            </div>
+            <div className='w-[80%] bg-gray-300 rounded-lg'>
+              Goals
+            </div>
+          </div>
+          <button className='bg-gray-300'>Start Session</button>
+        </div>
+        
       </div>
   )
 }
