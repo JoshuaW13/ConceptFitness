b5d2b3b6ee1efc2f209fd2eb40aaa0dac2a65f29
JoshuaW13 <josh.walters@shaw.ca>
2024-10-28 08:08:58 -0600

    Updated navbar to check if buttons props are null

--- conceptFitness/src/components/NavBar.jsx
+++ conceptFitness/src/components/NavBar.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import React from 'react'
-import '../App.css'
+import { useState } from 'react';
+import React from 'react';
+import '../App.css';
 
-function NavBar({FirstButton, SecondButton}) {
-
+function NavBar({ FirstButton, SecondButton }) {
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-100  w-full rounded-tr rounded-tl max-h-[12%]">
-        <FirstButton /> 
-        <SecondButton /> 
-  </nav>
-  )
+    <nav className="flex items-center justify-between p-4 bg-gray-100 w-full rounded-tr rounded-tl max-h-[12%]">
+      {FirstButton && <FirstButton />}
+      {SecondButton && <SecondButton />}
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
