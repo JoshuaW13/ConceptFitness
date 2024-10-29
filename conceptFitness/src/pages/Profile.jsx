1bba1419008c8633466b5b8de71fb4df16e8b3c9
Raisa Rafi <rafi.raisaar@gmail.com>
2024-10-28 22:51:00 -0600

    Added settings icon in the navbar

--- conceptFitness/src/pages/Profile.jsx
+++ conceptFitness/src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../App.css'
+import NavBar from '../components/navBar'
+import HomeButton from '../components/HomeButton'
+import SettingsButton from '../components/SettingsButton'
+
 function Profile() {
 
   return (
-    <div>
-        Profile
+    <div className='w-full h-full flex flex-col items-center'>
+        <NavBar FirstButton={HomeButton} SecondButton={SettingsButton}></NavBar>
     </div>
   )
 }
