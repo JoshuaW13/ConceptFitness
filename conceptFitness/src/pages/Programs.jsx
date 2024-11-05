1a45d973d44e09c9e5a28f97a9f39b210e84adf4
Nicholas Lam <100057820+Nicholas-Lam1@users.noreply.github.com>
2024-11-05 15:59:34 -0700

    Making program window

--- conceptFitness/src/pages/Programs.jsx
+++ conceptFitness/src/pages/Programs.jsx
@@ -1,14 +1,44 @@
-import React from 'react'
-import '../App.css'
-
+import React, { useState } from 'react';
+import '../App.css';
+import NavBar from '../components/NavBar';
+import HomeButton from '../components/HomeButton';
+import ExerciseLog from '../components/Program';
+import SessionLog from '../components/SessionLog';
+import DropDown from '../components/DropDown';
 
 function Programs() {
+  const sessionLogs = [];
+  const exerciseLogs2 = [];
+
+  for (let i = 0; i < 10; i++) {
+      exerciseLogs2.push(() => <ExerciseList key={i} />); 
+  }
+
+  for (let i = 0; i < 5; i++) {
+      sessionLogs.push(
+          <DropDown
+              key={i}
+              InitialComponent={SessionLog}
+              HiddenComponents={exerciseLogs2} 
+          />
+      );
+  }
 
   return (
-    <div>
-        Programs
-    </div>
-  )
+      <div className='w-full h-full flex flex-col items-center gap-2'>
+          <NavBar FirstButton={HomeButton} />
+          <div className='bg-black w-[75%] rounded-lg'>
+              <input type="text" placeholder='Search...' className='text-black bg-gray-300 w-full' />
+          </div>
+          <form>
+              <label htmlFor="date">Date:</label>
+              <input type="date" id="date" name="date" />
+          </form>
+          <div className='h-[75%] w-[75%] bg-gray-200 flex flex-col gap-2 overflow-y-auto m-3 scrollbar-hidden'>
+              {sessionLogs}
+          </div>
+      </div>
+  );
 }
 
 export default Programs
