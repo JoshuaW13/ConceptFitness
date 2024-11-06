0aeb427b3c31ac67f5004234ecd930b0f73c40f3
Nicholas Lam <100057820+Nicholas-Lam1@users.noreply.github.com>
2024-11-05 22:41:18 -0700

    Added scrolling to execise description

--- conceptFitness/src/components/ExerciseInfo.jsx
+++ conceptFitness/src/components/ExerciseInfo.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../App.css'
 import VideoBox from "../assets/VideoBox.png"
-
+import ExerciseImage from "../assets/ExerciseImage.png"
 
 function ExerciseInfo({onClick}) {
 
   return (
-    <div className='bg-gray-200 w-full rounded-lg font-semibold' onClick={onClick}>
+    <div className='bg-gray-200 w-full h-[80%] rounded-lg font-semibold overflow-y-auto scrollbar-hidden' onClick={onClick}>
         <p className='text-black'>Muscle: Maximus Gluteus</p>
         <p className='text-black'>Description:</p>
-        <p className='text-black pl-10 pr-10'>--------------------------------------------------------------------------------------------------------</p>
-        <img src={VideoBox} alt="" className='pl-10 pr-10 pb-5 pt-5' />
+        <p className='text-black pl-5 pr-5'>------------------------------------------------------------------------------------------------------------------------------------------------------------------------------</p>
+        <img src={VideoBox} alt="" className='p-5' />
+        <img src={ExerciseImage} alt="" className='p-5' />
     </div>
   )
 }
