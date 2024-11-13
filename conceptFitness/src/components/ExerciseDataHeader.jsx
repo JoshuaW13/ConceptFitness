3ba89f3dd569f474dd8b449eb6ee9f3835620f34
JoshuaW13 <josh.walters@shaw.ca>
2024-11-13 12:44:49 -0700

    Updated styling for logs pages to be cleaner

--- conceptFitness/src/components/ExerciseDataHeader.jsx
+++ conceptFitness/src/components/ExerciseDataHeader.jsx
@@ -1,31 +1,32 @@
 import React, { useState } from 'react';
-import '../App.css'
-import Popup from './Popup'
+import '../App.css';
+import Popup from './Popup';
 import Menu from "@mui/icons-material/Menu";
-import ExerciseDataPopup from "../components/ExerciseDataPopup"
+import ExerciseDataPopup from "../components/ExerciseDataPopup";
 
+function ExerciseDataHeader({ onClick }) {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
-function ExerciseDataHeader({onClick}) {
-const [isPopupVisible, setIsPopupVisible] = useState(false);
   return (
-    <div className='p-2 bg-gray-400 w-full rounded-t-lg font-semibold flex justify-between relative items-start'>
-        <div className='flex items-center flex-col justify-betweenvp-2 rounded-lg mb-4 'onClick={onClick}>
-            <p className='text-lg rounded-t-lg'>Exercise: Insert Name Here</p>
-            <p className='text-base'>Tags: #</p>
-            <p className='text-base'>Equipment: random machine</p>
-            
+    <div className="p-4 bg-gray-200 w-full rounded-t-lg shadow-md flex justify-between items-center relative cursor-pointer shadow-md transition-transform hover:scale-105 transform">
+      <div className="flex items-start flex-col justify-between gap-1" onClick={onClick}>
+        <p className="text-lg text-gray-800 font-semibold">Exercise: Insert Name Here</p>
+        <p className="text-sm text-gray-600">Tags: #</p>
+        <p className="text-sm text-gray-600">Equipment: random machine</p>
       </div>
-      <button 
-            className="flex items-center text-black bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded transition duration-200 focus:outline-none" 
-            onClick={() => setIsPopupVisible(!isPopupVisible)}
-            >
-            <Menu />
-            </button>
-        {isPopupVisible && (
-          <Popup onClick={() => setIsPopupVisible(false)} Content={ExerciseDataPopup}></Popup>
-        )}
+
+      <button
+        className="flex items-center text-gray-700 bg-gray-300 hover:bg-gray-400 px-4 py-2 rounded-full transition duration-200 focus:outline-none focus:ring-2 focus:ring-indigo-300"
+        onClick={() => setIsPopupVisible(!isPopupVisible)}
+      >
+        <Menu />
+      </button>
+
+      {isPopupVisible && (
+        <Popup onClick={() => setIsPopupVisible(false)} Content={ExerciseDataPopup} />
+      )}
     </div>
-  )
+  );
 }
 
-export default ExerciseDataHeader
+export default ExerciseDataHeader;
