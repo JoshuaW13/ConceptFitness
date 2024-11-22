346bc822326d5b5f0de2ec89b3fc75a1d99cf965
JoshuaW13 <josh.walters@shaw.ca>
2024-11-22 09:31:29 -0700

    Updated exercise names to be capitalized

--- conceptFitness/src/contexts/ExerciseCatalogueContext.jsx
+++ conceptFitness/src/contexts/ExerciseCatalogueContext.jsx
@@ -8,18 +8,18 @@ const ExerciseCatalogueContext = createContext();
 export const ExerciseCatalogueProvider = ({ children }) => {
   // Use React's useState hook to manage the list of exercises
   const [exercises, setExercises] = useState([
-    { id: 1, name: "pull up", equipment: "pull up bar", targetMuscle: "back, arms" },
-    { id: 2, name: "pushup", equipment: "NA", targetMuscle: "chest, triceps" },
-    { id: 3, name: "muscle up" , equipment: "pull up bar", targetMuscle: "back, chest, tricep"},
-    { id: 4, name: "squat" , equipment: "squat rack", targetMuscle: "hamstring, glutes"},
-    { id: 5, name: "bench press" , equipment: "bench and barbell", targetMuscle: "chest, arms"},
-    { id: 6, name: "dips" , equipment: "dip bar", targetMuscle: "chest, triceps"},
-    { id: 7, name: "cable row" , equipment: "cable row machine", targetMuscle: "back"},
-    { id: 8, name: "Bulgarian split squat" , equipment: "bench", targetMuscle: "hamstring, glutes"},
-    { id: 9, name: "Nordic Hamstring curl" , equipment: "foot anchor", targetMuscle: "hamstrings"},
+    { id: 1, name: "Pull Up", equipment: "pull up bar", targetMuscle: "back, arms" },
+    { id: 2, name: "Pushup", equipment: "NA", targetMuscle: "chest, triceps" },
+    { id: 3, name: "Muscle Up" , equipment: "pull up bar", targetMuscle: "back, chest, tricep"},
+    { id: 4, name: "Squat" , equipment: "squat rack", targetMuscle: "hamstring, glutes"},
+    { id: 5, name: "Bench Press" , equipment: "bench and barbell", targetMuscle: "chest, arms"},
+    { id: 6, name: "Dips" , equipment: "dip bar", targetMuscle: "chest, triceps"},
+    { id: 7, name: "Cable Row" , equipment: "cable row machine", targetMuscle: "back"},
+    { id: 8, name: "Bulgarian Split Squat" , equipment: "bench", targetMuscle: "hamstring, glutes"},
+    { id: 9, name: "Nordic Hamstring Curl" , equipment: "foot anchor", targetMuscle: "hamstrings"},
     { id: 10, name: "Military press" , equipment: "dumbbells", targetMuscle: "shoulders, arms"},
-    { id: 11, name: "Bicep curls" , equipment: "dumbbells", targetMuscle: "bicep"},
-    { id: 12, name: "Hammer curls" , equipment: "dumbbells", targetMuscle: "bicep, wrists"},
+    { id: 11, name: "Bicep Curls" , equipment: "dumbbells", targetMuscle: "bicep"},
+    { id: 12, name: "Hammer Curls" , equipment: "dumbbells", targetMuscle: "bicep, wrists"},
   ]);
 
   // Function to add an exercise
