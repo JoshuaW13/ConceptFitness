763070ddf383053647e0d95ad7fbe358f96f3f46
Nicholas Lam <146775051+FCE-NLam@users.noreply.github.com>
2024-11-24 18:16:37 -0700

    I hate the Date library

--- conceptFitness/src/components/Calender.jsx
+++ conceptFitness/src/components/Calender.jsx
@@ -22,14 +22,13 @@ function Calender() {
     console.log("Testing")
   }
 
-  const changeWeek = (direction) =>{
-    firstDate.setDate(firstDate.getDate() + (7 * direction))
-    lastDate.setDate(firstDate.getDate() + 6)
-    
+  const changeWeek = () =>{
+    firstDate.setTime(firstDate.getTime() + (7 * 86400000))
     setFirstDay(firstDate.toDateString().substring(4, 10))
-    setLastDay(lastDate.toDateString().substring(4, 10))
-
     console.log(firstDate)
+
+    lastDate.setTime(firstDate.getTime() + (6 * 86400000))
+    setLastDay(lastDate.toDateString().substring(4, 10))
     console.log(lastDate)
   }
 
@@ -47,45 +46,65 @@ function Calender() {
     <div id="calendar" className="w-[90%] h-[35%] bg-gray-50 rounded-lg shadow-lg flex flex-col items-start justify-between mt-6 pl-3 pr-3 pt-2 pb-2 border-gray border-2 overflow-x-auto scrollbar-none">        
       <div className='flex sticky top-0 w-full left-0 align-middle items-center justify-between pb-1'>
         <button className='bg-gray-300'>
-          <img src={ArrowLeftIcon} alt="" className="h-6 w-6 text-black"  onClick={() => changeWeek(-1)}/>
+          <img src={ArrowLeftIcon} alt="" className="h-6 w-6 text-black"/>
         </button>
         <p className='text-2xl font-semibold pb-1'>{firstDay} - {lastDay} </p>
         <button className='bg-gray-300'>
-          <img src={ArrowRightIcon} alt="" className="h-6 w-6 text-black" onClick={() => changeWeek(1)}/>
+          <img src={ArrowRightIcon} alt="" className="h-6 w-6 text-black" onClick={() => changeWeek()}/>
         </button>
       </div>
       <div className='flex h-[20%] w-[350%]'>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Sunday
-          <p className='static top-0 right-0'>test</p>
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Sunday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{firstDay.slice(-2)}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Monday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Monday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 1}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Tuesday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Tuesday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 2}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Wednesday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Wednesday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 3}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Thursday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Thursday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 4}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Friday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Friday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 5}</p>
         </div>
-        <div className='h-full w-full border-black border-2 text-l font-semibold'>
-          Saturday
+        <div className='relative h-full w-full border-black border-2'>
+          <p className='text-l font-semibold'>Saturday</p>
+          <p className='absolute -top-1 right-0.5 text-black '>{parseInt(firstDay.slice(-2)) + 6}</p>
         </div>
       </div>
       <div className='flex h-full w-[350%]'>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
-        <div className='h-full w-full border-black border-2'></div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+
+        </div>
+        <div className='h-full w-full border-black border-2'>
+          
+        </div>
       </div>
     </div>
   )
