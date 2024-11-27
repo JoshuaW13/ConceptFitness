e086d89b50c9f3a57f8bb21ff41006c9ad877e2c
JoshuaW13 <josh.walters@shaw.ca>
2024-11-27 09:51:42 -0700

    Added the ability to add tags to new programs

--- conceptFitness/src/components/CatalogueDrawerContent.jsx
+++ conceptFitness/src/components/CatalogueDrawerContent.jsx
@@ -9,10 +9,13 @@ import BookIcon from '../assets/BookIcon.png';
 import ExerciseInfoHeaderShort from '../components/ExerciseInfoHeaderShort';
 import ExerciseInfoShort from '../components/ExerciseInfoShort';
 import { useNavigate } from 'react-router-dom';
+import Tag from '../components/Tag';
 
 function CatalogueDrawerContent({ plannedExercises, setPlannedExercises }) {
   const navigate = useNavigate();
   const [activeItem, setActiveItem] = useState(null); // Track which item is currently hovered over
+  const [inputTag, setInputTag] = useState("");
+  const[tags, setTags]=useState([]);
 
   const navigatePrograms = () => {
     navigate("/programs");
@@ -42,8 +45,24 @@ function CatalogueDrawerContent({ plannedExercises, setPlannedExercises }) {
     },
   }));
 
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      addTag(); // Add tag when Enter is pressed
+      setInputTag("");
+    }
+  };
+
+  const addTag = ()=>{
+    console.log("Adding tag!"+inputTag);
+    setTags((prevTags) => [...prevTags, inputTag]);
+  }
+
+  const handleInputChange = (event) => {
+    setInputTag(event.target.value); // Update the state with the new value
+  };
+
   return (
-    <div className="p-3 h-[85%]">
+    <div className="p-3 h-full w-full flex-col">
       <div className="flex flex-row justify-between m-2">
         <input type="text" placeholder="Edit Program Name Here" className="w-[80%] h-8 text-black bg-gray-300 hover:bg-gray-400 pl-2 text-left text-lg rounded-md">
         </input>
@@ -51,9 +70,17 @@ function CatalogueDrawerContent({ plannedExercises, setPlannedExercises }) {
           <img src={BookIcon} alt="" className="p-1" />
         </button>
       </div>
+      <div className='overflow-x-auto h-[5%] flex gap-1 justify-center'>
+        {
+          tags.map((tag, index)=>(
+            <Tag key={index} text={tag}>
+            </Tag>
+          ))
+        }
+      </div>
       <div className="flex flex-row justify-between m-2">
         <p>Tags:</p>
-        <input type="text" className="w-[85%] text-black bg-gray-300 pl-2 rounded-md" placeholder="Arm, Upper Body, Triceps, etc..." />
+        <input onChange={handleInputChange} onKeyDown={handleKeyDown} value={inputTag} type="text" className="w-[85%] text-black bg-gray-300 pl-2 rounded-md" placeholder="Arm, Upper Body, Triceps, etc..." />
       </div>
 
       {/* DnD context with sensors */}
@@ -63,7 +90,7 @@ function CatalogueDrawerContent({ plannedExercises, setPlannedExercises }) {
         sensors={sensors}  // Using both Pointer and Touch sensors
       >
         <SortableContext items={plannedExercises.map((exercise) => exercise.id)} strategy={verticalListSortingStrategy}>
-          <div className='h-[100%] w-[93%] bg-gray-200 flex flex-col rounded-lg gap-2 overflow-y-auto m-3 scrollbar-hidden'>
+          <div className='w-[93%] flex-grow max-h-[78%] bg-gray-200 flex flex-col rounded-lg gap-2 overflow-y-auto m-3 scrollbar-hidden'>
             {plannedExercises.map((exercise) => (
               <SortableItem
                 key={exercise.id}
