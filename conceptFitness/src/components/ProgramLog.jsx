8d3e7cc49de2151683df1799e7648cbc852c242b
JoshuaW13 <josh.walters@shaw.ca>
2024-11-27 22:36:03 -0700

    Fixed tags causing overflow bug

--- conceptFitness/src/components/ProgramLog.jsx
+++ conceptFitness/src/components/ProgramLog.jsx
@@ -21,8 +21,8 @@ function ProgramLog({ onClick, name, tags, numExercises }) {
         <div className='flex flex-col w-full'>
           <p className='text-lg w-[92%]' onClick={onClick}>{name}</p>
 
-          {/* Only render the tags once, conditionally */}
-          <div className='flex gap-1 justify-center'>
+          {/* Tags is causing the problem */}
+          <div className='flex-wrap h-[5%] flex gap-1'>
             {tags && tags.length > 0 ? (
               tags.map((tag, index) => (
                 <Tag key={index} text={tag} />
@@ -32,23 +32,26 @@ function ProgramLog({ onClick, name, tags, numExercises }) {
             )}
           </div>
 
+
           <p className='text-base' onClick={onClick}># of Exercises: {numExercises}</p>
         </div>
 
-        <div className='flex flex-col h-full justify-between'>
-          <button className='flex w-6 h-6 bg-gray-300' onClick={navigateSession}>
-            <img src={StartIcon} alt="" className="p-1" />
-          </button>
-          <button
-            className='flex relative items-center text-black bg-gray-300 hover:bg-gray-400 w-6 h-6 rounded transition duration-200 focus:outline-none'
-            onClick={(e) => { setIsPopupVisible(!isPopupVisible); e.stopPropagation() }}
-          >
-            <Menu />
-            {isPopupVisible && (
-              <Popup className="relative" onClick={(e) => { setIsPopupVisible(false); e.stopPropagation() }} Content={ProgramLogPopup} />
-            )}
-          </button>
-        </div>
+          {/* The below div is overflowing outside it's parent!!! */}
+          <div className='flex flex-col justify-between w-auto'>
+            <button className='flex w-6 h-6 bg-gray-300' onClick={navigateSession}>
+              <img src={StartIcon} alt="" className="p-1" />
+            </button>
+            <button
+              className='flex relative items-center text-black bg-gray-300 hover:bg-gray-400 w-6 h-6 rounded transition duration-200 focus:outline-none'
+              onClick={(e) => { setIsPopupVisible(!isPopupVisible); e.stopPropagation() }}
+            >
+              <Menu />
+              {isPopupVisible && (
+                <Popup className="relative" onClick={(e) => { setIsPopupVisible(false); e.stopPropagation() }} Content={ProgramLogPopup} />
+              )}
+            </button>
+          </div>
+
       </div>
     </div>
   );
