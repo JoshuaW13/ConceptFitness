082f298ab955924654946d1803d845d3d6d3cdfd
JoshuaW13 <josh.walters@shaw.ca>
2024-12-04 22:20:12 -0700

    Made logs tabbed

--- conceptFitness/src/pages/ExerciseLogs.jsx
+++ conceptFitness/src/pages/ExerciseLogs.jsx
@@ -2,23 +2,30 @@ import React, { useState, useEffect } from 'react';
 import '../App.css';
 import NavBar from '../components/NavBar';
 import HomeButton from '../components/HomeButton';
+import ProfileButton from '../components/ProfileButton';
 import ExerciseDataHeader from '../components/ExerciseDataHeader';
 import DropDown from '../components/DropDown';
 import ExerciseData from '../components/ExerciseData';
-import LogNavbar from '../components/LogNavBar';
 import SearchBar from '../components/SearchBar';
 import { useExerciseLogContext } from '../contexts/ExerciseLogContext';
+import { useNavigate } from 'react-router-dom';
 import { useExerciseCatalogueContext } from '../contexts/ExerciseCatalogueContext';
 
 function ExerciseLogs() {
     const {exerciseLogs}=useExerciseLogContext();
     const {exercises}=useExerciseCatalogueContext();
-    console.log(exerciseLogs);
+    const navigate = useNavigate();
+
 
     const filteredList = []
     const [searchText, setSearchText] = useState("");
     const [searchState, setSearchState] = useState(true);
     const [filteredExerciseLog, setFilteredExerciseLog] = useState([])
+
+    const navigateSessions = () => {
+        navigate("/sessionLogs")
+    };
+
   
     const filterSearch = () => {
       if (searchText === "") {
@@ -42,23 +49,43 @@ function ExerciseLogs() {
     }, [searchState]);
 
     return (
-        <div className='w-full h-full flex flex-col items-center gap-2'>
-            <LogNavbar typeSelected={"exercise"} />
-            <SearchBar searchSetter={setSearchText} searchState={searchState} searchStateSetter={setSearchState} InitialText={"Pull-Up, Tricep, Barbell, etc..."}></SearchBar>
-            <div className='h-[75%] w-[75%] bg-gray-100 flex flex-col gap-4 overflow-y-auto m-3 scrollbar-hidden shadow-md'>
-                {filteredExerciseLog.map((exerciseLog,index)=>(
-                    <DropDown
-                        key={index}
-                        InitialComponent={ExerciseDataHeader}
-                        InitialProps={{
-                            exercise: exercises.find((exercise) => exercise.id == exerciseLog.exerciseId)
-                        }}
-                        HiddenComponents={ExerciseData}
-                        HiddenProps={{
-                            exercise: exerciseLog
-                        }}
-                    />
-                ))}
+        <div className='w-full h-full flex flex-col items-center scrollbar-hidden'>
+            <NavBar FirstButton={HomeButton} SecondButton={ProfileButton} PageTitle={"Logs"}></NavBar>
+            <div className="w-full h-full bg-gray-100 border-2 border-gray-300 rounded-lg shadow-md relative pt-4 overflow-y-auto flex flex-col justify-center"> {/* Main container with folder look */}
+            <div className="flex px-2">
+                <div
+                className={`cursor-pointer py-2 px-4 border-t-2 border-l-2 border-r-2 rounded-tl-lg rounded-tr-lg transition-all ease-in-out
+                    bg-white text-gray-700 shadow-lg`}
+                    onClick={() => navigateSessions()}
+                >
+                Sessions
+                </div>
+                <div
+                className={`cursor-pointer py-2 px-4 border-t-2 border-l-2 border-r-2 rounded-tl-lg rounded-tr-lg transition-all ease-in-out
+                    bg-[#E85A4F] text-white shadow-lg
+                }`}
+                >
+                Exercises
+                </div>
+            </div>
+            <div className="pt-6 px-4 pb-4 bg-white rounded-b-lg border-t-2 border-gray-300 flex-grow flex flex-col items-center scrollbar-hidden">
+                <SearchBar searchSetter={setSearchText} searchState={searchState} searchStateSetter={setSearchState} InitialText={"Pull-Up, Tricep, Barbell, etc..."}></SearchBar>
+                <div className='w-[75%] bg-gray-100 flex flex-col gap-4 overflow-y-auto m-3 scrollbar-hidden shadow-md'>
+                    {filteredExerciseLog.map((exerciseLog,index)=>(
+                        <DropDown
+                            key={index}
+                            InitialComponent={ExerciseDataHeader}
+                            InitialProps={{
+                                exercise: exercises.find((exercise) => exercise.id == exerciseLog.exerciseId)
+                            }}
+                            HiddenComponents={ExerciseData}
+                            HiddenProps={{
+                                exercise: exerciseLog
+                            }}
+                        />
+                    ))}
+                </div>
+            </div>
             </div>
         </div>
     );
--- conceptFitness/src/pages/SessionLogs.jsx
+++ conceptFitness/src/pages/SessionLogs.jsx
@@ -3,17 +3,26 @@ import '../App.css';
 import ExerciseLog from '../components/ExerciseLog';
 import SessionLog from '../components/SessionLog';
 import DropDown from '../components/DropDown';
-import LogNavbar from '../components/LogNavBar';
+import NavBar from '../components/NavBar';
 import SearchBar from '../components/SearchBar';
 import DateInput from '../components/DateInput';
 import { useSessionLogContext } from '../contexts/SessionLogContext';
 import { useProgramContext } from "../contexts/ProgramsContext"; 
 import { useExerciseCatalogueContext } from '../contexts/ExerciseCatalogueContext';
+import { useNavigate } from 'react-router-dom';
+import HomeButton from '../components/HomeButton';
+import ProfileButton from '../components/ProfileButton';
 
 function SessionLogs() {
+  const navigate = useNavigate();
+
     const {sessionLogs} = useSessionLogContext();
     const { programs } = useProgramContext();
     const { exercises }=useExerciseCatalogueContext();
+    const navigateExercises = () => {
+        navigate("/exerciseLogs")
+    };
+
 
     const constructExerciseLog = (exerciseRecord, index)=>{
         return() =>
@@ -56,24 +65,45 @@ function SessionLogs() {
     }, [searchState]);
 
     return (
-        <div className='w-full h-full flex flex-col items-center gap-2'>
-            <LogNavbar typeSelected={"session"}/>
-            <SearchBar searchSetter={setSearchText} searchState={searchState} searchStateSetter={setSearchState} InitialText={"Pull-Up, Tricep, Barbell, etc..."}></SearchBar>
-            <DateInput></DateInput>
-            <div className='h-[75%] w-[75%] bg-gray-100 flex flex-col gap-2 overflow-y-auto m-3 scrollbar-hidden shadow-md'>
-                {filteredSessionLog.map((sessionLog, index)=>(
-                    <DropDown
-                        key={index}
-                        InitialComponent={SessionLog}
-                        InitialProps={{
-                            sessionLog: sessionLog
-                        }}
-                        HiddenComponents={sessionLog.exerciseRecords.map((exerciseRecord, index) => {
-                            return constructExerciseLog(exerciseRecord, index);
-                        })}
-                    />
-                ))}
+        <div className='w-full h-full flex flex-col items-center gap-0'>
+            <NavBar FirstButton={HomeButton} SecondButton={ProfileButton} PageTitle={"Logs"}></NavBar>
+            <div className="w-full h-full bg-gray-100 border-2 border-gray-300 rounded-lg shadow-md relative pt-4 overflow-y-auto flex flex-col justify-center"> {/* Main container with folder look */}
+            <div className="flex px-2 ">
+                <div
+                className={`cursor-pointer py-2 px-4 border-t-2 border-l-2 border-r-2 rounded-tl-lg rounded-tr-lg transition-all ease-in-out
+                    bg-[#E85A4F] text-white shadow-lg`}
+                >
+                Sessions
+                </div>
+                <div
+                className={`cursor-pointer py-2 px-4 border-t-2 border-l-2 border-r-2 rounded-tl-lg rounded-tr-lg transition-all ease-in-out
+                    bg-white text-gray-700 shadow-lg
+                }`}
+                onClick={() => navigateExercises()}
+                >
+                Exercises
+                </div>
             </div>
+            <div className="pt-6 px-4 pb-4 bg-white rounded-b-lg border-t-2 border-gray-300 flex-grow flex flex-col items-center scrollbar-hidden">
+
+                <SearchBar searchSetter={setSearchText} searchState={searchState} searchStateSetter={setSearchState} InitialText={"Pull-Up, Tricep, Barbell, etc..."}></SearchBar>
+                <DateInput></DateInput>
+                <div className='w-[75%] bg-gray-100 flex flex-col gap-2 overflow-y-auto m-3 scrollbar-hidden shadow-md'>
+                    {filteredSessionLog.map((sessionLog, index)=>(
+                        <DropDown
+                            key={index}
+                            InitialComponent={SessionLog}
+                            InitialProps={{
+                                sessionLog: sessionLog
+                            }}
+                            HiddenComponents={sessionLog.exerciseRecords.map((exerciseRecord, index) => {
+                                return constructExerciseLog(exerciseRecord, index);
+                            })}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
         </div>
     );
 }
