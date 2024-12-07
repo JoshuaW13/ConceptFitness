04d8ba91a9f30b2301d9090d6f18df5f9dec9121
Nicholas Lam <146775051+FCE-NLam@users.noreply.github.com>
2024-12-06 23:09:49 -0700

    Made date select on goal default to the date selected and exercise reset when closed.

--- conceptFitness/src/components/AddGoalPopup.jsx
+++ conceptFitness/src/components/AddGoalPopup.jsx
@@ -7,14 +7,14 @@ import CalenderExercisePopup from './CalenderExercisePopup';
 import dayjs from 'dayjs'
 
 function AddGoalPopup({date, message, onConfirm, setIsExercisePopupVisible}) {
-  const { goalTypes, addGoal, selectedExercise } = useGoalContext()
+  const { goalTypes, addGoal, selectedExercise, setSelectedExercise } = useGoalContext()
   const { exercises } = useExerciseCatalogueContext()
 
   const [selectedGoal, setSelectedGoal] = useState("Select a Goal")
   const [valueLabel, setValueLabel] = useState("")
   const [unitLabel, setUnitLabel] = useState("")
   const [inputValue, setInputValue] = useState("")
-  const [dateValue, setDateValue] = useState(new Date())
+  const [dateValue, setDateValue] = useState(date)
 
   const handleGoalChange = (event) => {
     setSelectedGoal(event.target.value)
@@ -33,13 +33,20 @@ function AddGoalPopup({date, message, onConfirm, setIsExercisePopupVisible}) {
   useEffect(() => {
     console.log(dateValue)
   }, [dateValue])
+
+  useEffect(() => {
+    if (selectedGoal.includes("Weight Loss") || selectedGoal.includes("Weight Gain")) {
+      setSelectedExercise("Select an Exercise")
+      console.log(selectedExercise)
+    }
+  }, [selectedGoal])
   
   return (
     <div className="flex flex-col gap-2 h-full w-full">
       <p className='font-bold text-2xl'>{message}</p>
       <select id="goal-select" defaultValue="" onChange={handleGoalChange} className='h-10 rounded-md bg-[#EAE7DC] border-[3px] border-[#D8C3A5] font-bold text-[#E85A4F]' autoComplete='off'>
         <option value="" disabled>
-          - Select a Goal -
+          Select a Goal
         </option>
         {goalTypes.map((goalType) => (
           <option key={goalType.id} value={goalType.goal}>
@@ -47,7 +54,7 @@ function AddGoalPopup({date, message, onConfirm, setIsExercisePopupVisible}) {
           </option>
         ))}
       </select>
-      { (selectedGoal != "Select a Goal" || selectedGoal != "") && !(selectedGoal.includes("Weight Loss") || selectedGoal.includes("Weight Gain")) && (
+      {(selectedGoal != "Select a Goal" || selectedGoal != "") && !(selectedGoal.includes("Weight Loss") || selectedGoal.includes("Weight Gain")) && (
         <button onClick={() => {setIsExercisePopupVisible(true)}} className='h-10 rounded-md bg-[#EAE7DC] border-[3px] border-[#D8C3A5] font-bold text-[#E85A4F]'>{selectedExercise}</button>
       )}
       { valueLabel != "" && unitLabel != "" && (
@@ -64,11 +71,11 @@ function AddGoalPopup({date, message, onConfirm, setIsExercisePopupVisible}) {
             ></input>
             <p className='text-lg mx-3 justify-center items-center align-middle text-center'>{unitLabel}</p>
           </div>
-          <input value={dateValue} onChange={handleDateChange} type='date' className='h-10 rounded-md bg-[#EAE7DC] border-[3px] border-[#D8C3A5] font-bold text-[#E85A4F]' defaultValue={date}></input>
+          <input value={dayjs(date).format('YYYY-MM-DD')} onChange={handleDateChange} type='date' className='h-10 rounded-md bg-[#EAE7DC] border-[3px] border-[#D8C3A5] font-bold text-[#E85A4F]'></input>
         </div>
       )}
       { inputValue != "" && (
-        <button onClick={(e)=>{e.stopPropagation(); addGoal(goalTypes.find((g) => g.goal == selectedGoal).id, exercises.find((ex) => ex.name == selectedExercise).id, inputValue, dayjs(dateValue).format('MMM D, YYYY')); onConfirm();}} className="bg-gray-300 text-black pl-2 pr-2">Add New Goal!</button>
+        <button onClick={(e)=>{e.stopPropagation(); addGoal(goalTypes.find((g) => g.goal == selectedGoal).id, exercises.find((ex) => ex.name == selectedExercise).id, inputValue, dayjs(dateValue).format('MMM D, YYYY')); setSelectedExercise("Select an Exercise"); onConfirm();}} className="bg-gray-300 text-black pl-2 pr-2">Add New Goal!</button>
       )}
     </div>
   )
