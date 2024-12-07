c09f5be8c6f9eed542af3f592121b61c1e94b020
JoshuaW13 <josh.walters@shaw.ca>
2024-12-07 11:34:16 -0700

    Removed home button on the homepage
    it does nothing lol

--- conceptFitness/src/components/NavBar.jsx
+++ conceptFitness/src/components/NavBar.jsx
@@ -6,15 +6,19 @@ function NavBar({ FirstButton, SecondButton, OtherButtons, PageTitle }) {
   const otherButtonsArray = Array.isArray(OtherButtons) ? OtherButtons : [];
 
   return (
-    <nav className="navbar p-2">
-      {FirstButton && <FirstButton />}
-      {otherButtonsArray.map((getButtonProps, index) => {
-        // Invoke the function to get the component and props
-        const { component: ButtonComponent, props } = getButtonProps();
-        return <ButtonComponent key={index} {...props} />;
-      })}
-      {PageTitle&& <h2 className=" flex justify-center items-center text-xl font-bold text-gray-800">{PageTitle}</h2>}
-      {SecondButton && <SecondButton />}
+    <nav className="navbar p-2 flex justify-between items-center">
+      <div className="left-buttons flex items-center">
+        {FirstButton && <FirstButton />}
+        {otherButtonsArray.map((getButtonProps, index) => {
+          // Invoke the function to get the component and props
+          const { component: ButtonComponent, props } = getButtonProps();
+          return <ButtonComponent key={index} {...props} />;
+        })}
+        {PageTitle && (
+          <h2 className="text-xl font-bold text-gray-800 mx-4">{PageTitle}</h2>
+        )}
+      </div>
+      {SecondButton && <div className="ml-auto">{<SecondButton />}</div>}
     </nav>
   );
 }
