9564bd65aaf9d49c2505131ef9e10e64bb5e828c
JoshuaW13 <josh.walters@shaw.ca>
2024-12-07 17:36:40 -0700

    Fixed exercise page text

--- conceptFitness/src/components/ExerciseInfo.jsx
+++ conceptFitness/src/components/ExerciseInfo.jsx
@@ -6,9 +6,9 @@ import ExerciseImage from "../assets/ExerciseImage.png"
 function ExerciseInfo({onClick, description}) {
 
   return (
-    <div className='bg-[#E8E7DC] w-full h-[100%] rounded-lg font-semibold overflow-y-auto scrollbar-hidden' onClick={onClick}>
-        <p className='text-black text-xl bg-[#EAE7DC]'>Description:</p>
-        <p className='text-black pl-2 pr-3 text-sm bg-[#EAE7DC]'>{description}</p>
+    <div className='bg-[#E8E7DC] w-full h-[100%] rounded-lg overflow-y-auto scrollbar-hidden' onClick={onClick}>
+        <p className='text-black font-semibold text-xl bg-[#EAE7DC]'>Description:</p>
+        <p className='text-black pl-2 pr-3 text-lg bg-[#EAE7DC]'>{description}</p>
         <img src={VideoBox} alt="" className='p-5' />
         <img src={ExerciseImage} alt="" className='p-5' />
     </div>
